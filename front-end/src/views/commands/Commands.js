10db73cbf8e1dfe980bc53872b574dc026827714
zaineb.bachouch <zaineb.bachouch@esprit.tn>
2024-05-03 15:26:43 +0100

    fix some link commands

--- front-end/src/views/commands/Commands.js
+++ front-end/src/views/commands/Commands.js
@@ -2,64 +2,78 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import CompleteCommand from '../../components/sidenav/completeCommand'; // Correct the import statement
+import SideBar from '../../components/sidebar/SideBar';
+import TopBar from "../../components/sidenav/TopNav";
+
+
 
 function Commands() {
-  console.log('Commands component rendered');
-  const [loading, setLoading] = useState(true);
-  const location = useLocation();
-  //console.log('location.search:', location.search);
-  const searchParams = new URLSearchParams(location.search);
-  const id = searchParams.get('idcommand');
+    console.log('Commands component rendered');
+    const [loading, setLoading] = useState(true);
+    const location = useLocation();
+    //console.log('location.search:', location.search);
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get('idcommand');
 
- // console.log('Extracted id:', id);
-  const [commandData, setCommandData] = useState(null); // Define commandData state variable
+    // console.log('Extracted id:', id);
+    const [commandData, setCommandData] = useState(null); // Define commandData state variable
 
-  useEffect(() => {
-    const currentCommandeId = localStorage.getItem('currentCommandeId');
+    useEffect(() => {
+        const currentCommandeId = localStorage.getItem('currentCommandeId');
 
-    if (id) {
-      // Fetch command details using the id from query parameter
-     console.log('fetch id uselocation ',id)
-      fetchData(id);
-    } else if (currentCommandeId) {
-      // Fetch command details using the currentCommandeId from local storage
-      fetchData(currentCommandeId);
-      console.log('localStorage  currentCommandeId',currentCommandeId)
-    } else {
-      setLoading(false);
-    }
-  }, [id]);
+        if (id) {
+            // Fetch command details using the id from query parameter
+            console.log('fetch id uselocation ', id)
+            fetchData(id);
+        } else if (currentCommandeId) {
+            // Fetch command details using the currentCommandeId from local storage
+            fetchData(currentCommandeId);
+            console.log('localStorage  currentCommandeId', currentCommandeId)
+        } else {
+            setLoading(false);
+        }
+    }, [id]);
 
-  const fetchData = async (currentCommandeId) => {
-    try {
-     // console.log('Fetching data for ID:', currentCommandeId);
-      const token = localStorage.getItem('token');
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const response = await axios.post('http://127.0.0.1:5000/api/completeCommand', { currentCommandeId }, config);
-      console.log('Fetched data:', response.data);
-      setCommandData(response.data); // Set the command state variable to the fetched data
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setLoading(false);
-    }
-  };
+    const fetchData = async (currentCommandeId) => {
+        try {
+            // console.log('Fetching data for ID:', currentCommandeId);
+            const token = localStorage.getItem('token');
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
+            const response = await axios.post('http://127.0.0.1:5000/api/completeCommand', { currentCommandeId }, config);
+            console.log('Fetched data:', response.data);
+            setCommandData(response.data); // Set the command state variable to the fetched data
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+        }
+    };
+    const role = localStorage.getItem('role');
+    return (
+        <div>
+            {loading ? (
+                <div>Loading...</div>
+            ) : commandData ? (
+                <CompleteCommand commandData={commandData} />
+            ) : null}
+            {role !== 'client' &&
+                <div className="d-flex">
+                    {/* Sidebar component */}
+                    <SideBar />
 
-  return (
-    <div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : commandData ? (
-        <CompleteCommand commandData={commandData} />
-      ) : (
-        <div>No command details found.</div>
-      )}
-    </div>
-  );
+                    <div className="container-fluid flex-column">
+                        {/* TopBar component */}
+                        <TopBar />
+
+                        <div className="container-fluid p-2">
+                            <div>Hello, I am an admin or employee.</div>
+                        </div></div></div>
+            }
+        </div>
+    );
 }
-
 export default Commands;
